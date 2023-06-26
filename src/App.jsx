6f52b0b6e68ecaa1cffc72c757467b34f408f79b
ntu002 <ngoctubn2002@gmail.com>
2023-06-26 20:06:06 +0700

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import PeopleDashboard from "./components/People/PeopleDashboard";
 import AbsentDashboard from "./components/Absent/AbsentDashboard";
 import ResidentDashboard from "./components/Resident/ResidentDashboard";
 function App() {
-  return <ResidentDashboard></ResidentDashboard>;
+  //return <ResidentDashboard></ResidentDashboard>;
+  return <div>
+    <h1>Login Form</h1>
+    <Login />
+  </div>
 }
 
 export default App;
