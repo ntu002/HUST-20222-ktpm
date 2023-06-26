0764fa5392279f4e625843c671af42107af577eb
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-06-26 12:20:02 +0700

    them tam tru tam vang

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import HouseholdDashboard from "./components/Household/HouseholdDashboard";
 import Login from "./components/Login/Login";
 import PeopleDashboard from "./components/People/PeopleDashboard";
 import AbsentDashboard from "./components/Absent/AbsentDashboard";
+import ResidentDashboard from "./components/Resident/ResidentDashboard";
 function App() {
-  return <AbsentDashboard></AbsentDashboard>;
+  return <ResidentDashboard></ResidentDashboard>;
 }
 
 export default App;
