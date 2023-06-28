a2b8c956823af461131c1808c7f8d432310f2de4
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-06-28 22:02:06 +0700

    main

--- src/components/Home/Sidebar.jsx
+++ src/components/Home/Sidebar.jsx
@@ -2,8 +2,10 @@ import Styles from "./Sidebar.module.css";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faPerson } from "@fortawesome/free-solid-svg-icons";
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faGear, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import logo from "./logo.png";
 function Sidebar() {
     
     //handleClick(event){
@@ -20,14 +22,17 @@ function Sidebar() {
     }
         return (
             <div class = {Styles.menu}>
-                <label>Quản Lý Dân Cư</label>
+                <div class={Styles.brand}>
+                    <img src={logo} alt="Logo" />
+                    <label>Quản Lý Dân Cư</label>
+                </div>
                 <ul>
-                    <li><a id ="Home" /*onClick={handleClick}*/ >Trang Chủ</a></li>
-                    <li ><a id = "Househole" onClick={handleClick} >
-                    Quản Lý Hộ Khẩu</a> </li>
-                    <li><a id = "People" /*onClick={this.handleClick}*/>Quản Lý Nhân Khẩu</a></li>
-                    <li><a id = "Meeting" /*onClick={this.handleClick}*/>Quản Lý Cuộc Họp</a></li>
-                    <li><a id = "account" /*onClick={this.handleClick}*/ >Tài Khoản</a></li>
+                    <li><a id ="Home" /*onClick={handleClick}*/ ><i><FontAwesomeIcon icon={faHouse} /></i><span> Trang Chủ </span></a></li>
+                    <li ><a id = "Househole" onClick={handleClick} ><i><FontAwesomeIcon icon={faPeopleGroup} /></i>
+                    <span>Quản Lý Hộ Khẩu</span></a> </li>
+                    <li><a id = "People" /*onClick={this.handleClick}*/><i><FontAwesomeIcon icon={faPerson} /></i><span>Quản Lý Nhân Khẩu</span></a></li>
+                    <li><a id = "Meeting" /*onClick={this.handleClick}*/><i><FontAwesomeIcon icon={faCalendar} /></i><span>Quản Lý Cuộc Họp</span></a></li>
+                    <li><a id = "account" /*onClick={this.handleClick}*/ ><i><FontAwesomeIcon icon={faGear} /></i><span>Tài Khoản</span></a></li>
                 </ul>
             </div>
         )
