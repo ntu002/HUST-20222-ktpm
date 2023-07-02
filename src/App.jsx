c4f13c55645b64429fc2b274b49220ab95e8ef42
ThanhTien3103 <thanhtien31032002@gmail.com>
2023-07-02 17:29:33 +0700

    Duong update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import EditPeople from "./components/People/EditPeople";
 import Sidebar from "./components/Home/Sidebar";
 import Topbar from "./components/Home/Topbar";
 import Styles from "./App.module.css";
+import MainDashBoard from "./components/Home/MainDashBoard";
 import { useState } from "react";
 function App() {
   const [isLogin, setLogined] = useState(false);
@@ -31,14 +32,12 @@ function App() {
           <Topbar ></Topbar>
           <div class={Styles.main}>
 
-            <AddHouseHold></AddHouseHold>
-
+            
           
           {(target === "People") && <PeopleDashboard></PeopleDashboard>}
           {(target === "HouseHold") && <HouseholdDashboard></HouseholdDashboard>}
-
-          {(target === "Home") && <PeopleDashboard></PeopleDashboard>}
-          {(target === "Meeting") && <HouseholdDashboard></HouseholdDashboard>}
+          {(target === "Home") &&<HouseholdDashboard></HouseholdDashboard>}
+          {(target === "Meeting") &&  <MainDashBoard></MainDashBoard>}
           {(target === "Account") && <HouseholdDashboard></HouseholdDashboard>} 
 
 
