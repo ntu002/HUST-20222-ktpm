10a7590f53eaac386bfc90a7c2a13e236bf11aa7
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-07-02 20:08:36 +0700

    m

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,7 @@ import EditPeople from "./components/People/EditPeople";
 import Sidebar from "./components/Home/Sidebar";
 import Topbar from "./components/Home/Topbar";
 import Styles from "./App.module.css";
-<<<<<<< HEAD
-import HouseHoldDescription from "./components/Household/HouseholdDescription";
-import AddRelationship from "./components/Household/popup/AddRelationship";
-import AddHouseHold from "./components/Household/AddHouseHold";
-import EditHouseHold from "./components/Household/EditHouseHold";
-import CovidForm from "./components/Home/CovidForm";
-=======
 import MainDashBoard from "./components/Home/MainDashBoard";
->>>>>>> 389036c7bc3e40317a83a4f914b3861bfad65b2f
 import { useState } from "react";
 function App() {
   const [isLogin, setLogined] = useState(false);
@@ -42,25 +34,16 @@ function App() {
         <div class={Styles.right}>
           <Topbar ></Topbar>
           <div class={Styles.main}>
-<<<<<<< HEAD
-          {/* <HouseHoldDescription></HouseHoldDescription>           */}
-          {/* {(target === "People") && <PeopleDashboard></PeopleDashboard>} */}
-          {/* {(target === "HouseHold") && <HouseholdDashboard></HouseholdDashboard>} */}
-          {/* <AddHouseHold></AddHouseHold> */}
-          {/* <EditHouseHold></EditHouseHold> */}
-          <CovidForm></CovidForm>
-=======
 
             
           
-          {(target === "People") && <PeopleDashboard></PeopleDashboard>}
+          {/* {(target === "People") && <PeopleDashboard></PeopleDashboard>}
           {(target === "HouseHold") && <HouseholdDashboard></HouseholdDashboard>}
           {(target === "Home") &&<HouseholdDashboard></HouseholdDashboard>}
           {(target === "Meeting") &&  <MainDashBoard></MainDashBoard>}
-          {(target === "Account") && <HouseholdDashboard></HouseholdDashboard>} 
+          {(target === "Account") && <HouseholdDashboard></HouseholdDashboard>}  */}
+          <PeopleDashboard></PeopleDashboard>
 
-
->>>>>>> 389036c7bc3e40317a83a4f914b3861bfad65b2f
           </div>
         </div>
         <div class={Styles.left}>
